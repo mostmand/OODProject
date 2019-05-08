2032beae4e7f29e49527aea63b72d0c53384b059
MH Mostmand <mostmand@hotmail.com>
2019-05-08 14:08:04 +0430

    Added executable architectural baseline

---
+++ Prototype/OODProto/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OODProto.MvcApplication" Language="C#" %>
---
+++ Prototype/OODProto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OODProto.Startup))]
+namespace OODProto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
