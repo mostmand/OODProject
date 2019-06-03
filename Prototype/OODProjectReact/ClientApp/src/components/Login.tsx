b465fe4d89d55f70d2da604459b7786d81de3fbe
MH Mostmand <mostmand@hotmail.com>
2019-06-03 13:10:23 +0430

    Correct login screen

--- Prototype/OODProjectReact/ClientApp/src/components/Login.tsx
+++ Prototype/OODProjectReact/ClientApp/src/components/Login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FetchUtil } from "./utilities/FetchUtil";
 import { CookieUtil } from "./utilities/CookieUtil";
-import { Container, Header, Message, Form, Label, Button, Input, Segment } from "semantic-ui-react";
+import { Container, Header, Message, Form, Label, Button, Input, Segment, Grid } from "semantic-ui-react";
 
 interface IProps {
     loggedIn: boolean;
@@ -56,25 +56,44 @@ export default class LoginPage extends React.Component<IProps, ILocalState>{
         if (this.state.loginFailed)
             loginState = <Message error content="نام کاربری یا رمز عبور اشتباه است" />;
         return (
-            <Segment>
-                <Header>
-                    ورود
-                </Header>
-                {loginState}
+            <Grid textAlign="center">
+                <Grid.Row columns={1} textAlign="right">
+                    <Grid.Column width={6}>
+                        <Segment color="teal">
+                            <Header>
+                                ورود
+                            </Header>
+                            {loginState}
 
-                <Form>
-                    <Form.Field>
-                        <Label htmlFor="username">نام کاربری</Label>
-                        <Input type="text" id="username" ref={this.userName} onKeyUp={this.handleEnter} />
-                    </Form.Field>
-                    <Form.Field>
-                        <Label htmlFor="password">رمز عبور</Label>
-                        <Input type="password" id="password" className="form-control" ref={this.password} onKeyUp={this.handleEnter} />
-                    </Form.Field>
-
-                    <Button className="button btn my-button" ref={this.signInButton} onClick={this.submit}>ورود</Button>
-                </Form>
-            </Segment>
+                            <Form>
+                                <Grid>
+                                    <Grid.Row columns={1} centered>
+                                        <Grid.Column textAlign="right">
+                                            <Form.Field>
+                                                <Label basic color="teal" htmlFor="username">نام کاربری</Label>
+                                                <Input fluid type="text" id="username" ref={this.userName} onKeyUp={this.handleEnter} />
+                                            </Form.Field>
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                    <Grid.Row columns={1} centered>
+                                        <Grid.Column textAlign="right">
+                                            <Form.Field>
+                                                <Label basic color="teal" htmlFor="password">رمز عبور</Label>
+                                                <Input fluid type="password" id="password" ref={this.password} onKeyUp={this.handleEnter} />
+                                            </Form.Field>
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                    <Grid.Row columns={1} centered>
+                                        <Grid.Column>
+                                            <Button fluid color="teal" className="no-margin-btn" ref={this.signInButton} onClick={this.submit}>ورود</Button>
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Grid>
+                            </Form>
+                        </Segment>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
         );
     }
 
