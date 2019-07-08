d987258bf7a7c474abf465c220e0f0cf1b1d55e4
Mohammad Haghighat <mhaghighat@rayana.ir>
2019-07-08 13:52:46 +0430

    اضافه شدن اینترفیس گزارشگیری

---
+++ Prototype/OODProjectReact/Controllers/Report/IReport.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OODProjectReact.Controllers.Report
+{
+    interface IReportGenerator
+    {
+        Report<GoodQuantityReportItem> GetGoodQuantityReport(DateTime start, DateTime end);
+
+        Report<CustomerInvoicesReportItem> GetCustomerInvoiceReport(DateTime start, DateTime end);
+    }
+
+    public class Report<T> where T : IReportItem
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        List<T> ReportItems { get; set; }
+    }
+
+    public interface IReportItem
+    {
+
+    }
+
+    public class GoodQuantityReportItem : IReportItem
+    {
+        public string GoodName { get; set; }
+
+        public int QuantityAtStartOfPeriod { get; set; }
+
+        public int QuantityAtEndOfPeriod { get; set; }
+
+        public int QuantitySold { get; set; }
+    }
+
+    public class CustomerInvoicesReportItem : IReportItem
+    {
+        public string CustomerName { get; set; }
+
+        public int InvoicesCount { get; set; }
+
+        public long TotalPriceOfInvoices { get; set; }
+    }
+
+}
